ab96cce0e264c2bdc1a263afce4e365060a4e24a
Stefan Haack <shaack@gmail.com>
2019-07-29 17:58:27 +0200

    works

--- src/linkable.js
+++ src/linkable.js
@@ -1,5 +1,12 @@
 function linkable() {
-    document.addEventListener("DOMContentLoaded", (event) => {
-        console.log("Content loaded")
+    document.addEventListener("DOMContentLoaded", () => {
+        const linkables = document.getElementsByClassName("linkable")
+        for (const linkable of linkables) {
+            linkable.style.cursor = "pointer"
+            linkable.addEventListener("click", (event) => {
+                location.href = event.target.getAttribute("data-href")
+            })
+        }
     })
 }
+linkable()
