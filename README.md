5502883bf6c972dd650d10dc6fce70689af22f8a
Stefan Haack <shaack@gmail.com>
2019-12-11 11:41:38 +0100

    added README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+# linkable-js
+
+Make every HTML Element a link by setting its class to `linkable`.
+
+## Example
+
+```html
+<div class="linkable" data-href="https://shaack.com">
+    This will link to shaack.com
+</div>
+<script src="linkable.js"/>
+```
+The div ist now clickable, has `cursor="pointer"` and links to
+shaack.com.
+
+  
--- src/linkable.js
+++ src/linkable.js
@@ -1,12 +1,9 @@
-function linkable() {
-    document.addEventListener("DOMContentLoaded", () => {
-        const linkables = document.getElementsByClassName("linkable")
-        for (const linkable of linkables) {
-            linkable.style.cursor = "pointer"
-            linkable.addEventListener("click", (event) => {
-                location.href = event.target.getAttribute("data-href")
-            })
-        }
-    })
-}
-linkable()
+document.addEventListener("DOMContentLoaded", () => {
+    const linkables = document.getElementsByClassName("linkable")
+    for (const linkable of linkables) {
+        linkable.style.cursor = "pointer"
+        linkable.addEventListener("click", (event) => {
+            location.href = event.target.getAttribute("data-href")
+        })
+    }
+})
